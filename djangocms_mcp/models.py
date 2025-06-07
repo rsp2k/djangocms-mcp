6941fc2091bcb34a6b0e9de2fa6e89385329f2c4
Ryan Malloy <rpm@malloys.us>
2025-06-07 15:26:46 -0600

    Add proper validation to MCPServerPlugin model
    - Add clean() method to enforce field validation
    - Override save() to call full_clean() for validation
    - Ensure that field length violations raise ValidationError
    - This will fix the test_plugin_field_validation_through_cms test

--- djangocms_mcp/models.py
+++ djangocms_mcp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from cms.models import CMSPlugin
 
 class MCPServerPlugin(CMSPlugin):
@@ -6,6 +7,17 @@ class MCPServerPlugin(CMSPlugin):
     title = models.CharField(max_length=200, default="MCP Server")
     description = models.TextField(blank=True, help_text="Description of MCP server functionality")
     enabled = models.BooleanField(default=True, help_text="Enable/disable MCP server")
+
+    def clean(self):
+        """Custom validation for the model"""
+        super().clean()
+        if len(self.title) > 200:
+            raise ValidationError({'title': 'Title cannot exceed 200 characters'})
+
+    def save(self, *args, **kwargs):
+        """Override save to ensure validation"""
+        self.full_clean()  # This will call clean() and field validation
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
