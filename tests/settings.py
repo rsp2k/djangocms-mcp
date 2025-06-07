7232f7e8d5a982a59a6e3f9094b0e59f24aa1c8a
Ryan Malloy <rpm@malloys.us>
2025-06-07 07:44:14 -0600

    Update test settings to include template directory path

--- tests/settings.py
+++ tests/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'tests.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'tests' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
